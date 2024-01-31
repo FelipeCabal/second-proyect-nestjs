fe12afd122689978f94341009d40f74bbf44c469
Felipe Cabal <phelipekabbal_85@hotmail.com>
2024-01-31 02:13:35 -0500

    Configuracion y variables de entorno

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as morgan from 'morgan';
+import * as dotenv from 'dotenv';
+import { CORS } from './constants';
+
+// Cargar variables de entorno con dotenv
+dotenv.config({ path: `.develop.env` });
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(morgan('dev'))
+
+  const configService = app.get(ConfigService)
+
+  app.enableCors(CORS)
+
+  app.setGlobalPrefix('api')
+  console.log(process.env.NODE_ENV)
+  console.log(configService.get('PORT'))
+  await app.listen(configService.get('PORT'));
+  console.log(`Aplication running on: ${await app.getUrl()}`)
 }
 bootstrap();
+
